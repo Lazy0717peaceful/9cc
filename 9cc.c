a86c91781c9f8d3f69c4eb8afc692d14eabe07ee
Reiji Tajima <293389173+Lazy0717peaceful@users.noreply.github.com>
2026-06-30 06:40:06 +0000

    エラー箇所の報告

--- 9cc.c
+++ 9cc.c
@@ -106,6 +106,23 @@ Token *tokenize(char *p) {
   return head.next;
 }
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     error("引数の個数が正しくありません");
