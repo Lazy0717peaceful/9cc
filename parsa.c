619df6acfaa71d7820b8ce148df1cc872bb0bfdd
Reiji Tajima <293389173+Lazy0717peaceful@users.noreply.github.com>
2026-07-17 04:59:54 +0000

    ファイル分けを整理した

--- parsa.c
+++ parsa.c
@@ -6,104 +6,98 @@
 #include <string.h>
 #include "9cc.h"
 
-// 現在着目しているトークン
-Token *token;
-
-// 入力プログラム
-char *user_input;
-
-void error(char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
 }
 
-bool consume(char *op) {
-  if (token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len))
-    return false;
-  token = token->next;
-  return true;
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
 }
 
-void expect(char *op) {
-  if (token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len))
-    error_at(token->str, "expected \"%s\"", op);
-  token = token->next;
+Node *expr() {
+  return equality();
 }
 
-int expect_number() {
-  if (token->kind != TK_NUM)
-    error("数ではありません");
-  int val = token->val;
-  token = token->next;
-  return val;
-}
+Node *equality() {
+  Node *node = relational();
 
-bool at_eof() {
-  return token->kind == TK_EOF;
-}
-
-bool startswith(char *p, char *q) {
-  return memcmp(p, q, strlen(q)) == 0;
-}
-
-Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
-  Token *tok = calloc(1, sizeof(Token));
-  tok->kind = kind;
-  tok->str = str;
-  tok->len = len;
-  cur->next = tok;
-  return tok;
-}
-
-Token *tokenize(char *p) {
-  Token head;
-  head.next = NULL;
-  Token *cur = &head;
-
-  while (*p) {
-    // 空白文字をスキップ
-    if (isspace(*p)) {
-      p++;
-      continue;
-    }
-
-    if (startswith(p, "==") || startswith(p, "!=") || startswith(p, "<=") || startswith(p, ">=")) {
-      cur = new_token(TK_RESERVED, cur, p, 2);
-      p += 2;
-      continue;
-    }
-
-
-    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')' || *p == '<' || *p == '>') {
-      cur = new_token(TK_RESERVED, cur, p++,1);
-      continue;
-    }
-
-    if (isdigit(*p)) {
-      cur = new_token(TK_NUM, cur, p, 0);
-      cur->val = strtol(p, &p, 10);
-      continue;
-    }
-
-    error("トークナイズできません");
+  for (;;) {
+    if(consume("=="))
+      node = new_node(ND_EQU, node, relational());
+    else if(consume("!="))
+      node = new_node(ND_NEQ, node, relational());
+    else
+      return node;
   }
 
-  new_token(TK_EOF, cur, p, 0);
-  return head.next;
 }
 
-void error_at(char *loc, char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
+Node *relational() {
+  Node *node = add();
 
-  int pos = loc - user_input;
-  fprintf(stderr, "%s\n", user_input);
-  fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
-  fprintf(stderr, "^ ");
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
+  for (;;) {
+    if(consume("<"))
+      node = new_node(ND_SML, node, add());
+    else if(consume("<="))
+      node = new_node(ND_LEQ, node, add());
+    else if(consume(">"))
+      node = new_node(ND_SML, add(), node);
+    else if(consume(">="))
+      node = new_node(ND_LEQ, add(), node);
+    else
+      return node;
+  }
+}
+
+Node *add() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume("+"))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume("-"))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = unary();
+
+  for (;;) {
+    if (consume("*"))
+      node = new_node(ND_MUL, node, unary());
+    else if (consume("/"))
+      node = new_node(ND_DIV, node, unary());
+    else
+      return node;
+  }
+}
+
+Node *unary() {
+  if (consume("+"))
+    return primary();
+  if (consume("-"))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
+}
+
+Node *primary() {
+  // 次のトークンが"("なら、"(" expr ")"のはず
+  if (consume("(")) {
+    Node *node = expr();
+    expect(")");
+    return node;
+  }
+
+  // そうでなければ数値のはず
+  return new_node_num(expect_number());
 }
